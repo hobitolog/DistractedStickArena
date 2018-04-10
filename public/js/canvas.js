9e0f008a552f228073f5c0394e9b49be92e77d38
Yuunai <minta96@gmail.com>
2018-04-10 19:34:03 +0200

    Background and stickman added

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,36 +1,55 @@
-window.onload = function (){
+window.onload = function () {
 
-// create a wrapper around native canvas element (with id="c")
-var canvas = new fabric.Canvas('c');
+    // create a wrapper around native canvas element (with id="c")
+    var canvas = new fabric.Canvas('c');
 
-fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
+    fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 
-fabric.loadSVGFromURL('../svg/Circus.svg', function(objects, options) {
-    var obj = fabric.util.groupSVGElements(objects, options);
-    obj.scale(1.75);
-    obj.set({left: 344, top: 100});
-canvas.add(obj);
-})
+    fabric.loadSVGFromURL('../svg/Background.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.set({ left: 360, top: 243 })
+        obj.selectable = false;
+        canvas.add(obj);
+    })
 
-fabric.loadSVGFromURL('../svg/Inn.svg', function(objects, options) {
-    var obj = fabric.util.groupSVGElements(objects, options);
-    obj.scale(1.75);
-    obj.set({left: 192, top: 224});
-canvas.add(obj);
-})
+    fabric.loadSVGFromURL('../svg/Circus.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.scale(1.75);
+        obj.set({ left: 344, top: 100 });
+        obj.selectable = false;
+        canvas.add(obj);
+    })
 
-fabric.loadSVGFromURL('../svg/Blacksmith.svg', function(objects, options) {
-    var obj = fabric.util.groupSVGElements(objects, options);
-    obj.scale(1.75);
-    obj.set({left: 128, top: 374});
-canvas.add(obj);
-})
+    fabric.loadSVGFromURL('../svg/Inn.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.scale(1.75);
+        obj.set({ left: 192, top: 224 });
+        obj.selectable = false;
+        canvas.add(obj);
+    })
 
-fabric.loadSVGFromURL('../svg/Statue.svg', function(objects, options) {
-    var obj = fabric.util.groupSVGElements(objects, options);
-    obj.scale(3);
-    obj.set({left: 512, top: 256});
-canvas.add(obj);
-})
+    fabric.loadSVGFromURL('../svg/Blacksmith.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.scale(1.75);
+        obj.set({ left: 128, top: 374 });
+        obj.selectable = false;
+        canvas.add(obj);
+    })
+
+    fabric.loadSVGFromURL('../svg/Statue.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.scale(2);
+        obj.set({ left: 532, top: 276 });
+        obj.selectable = false;
+        canvas.add(obj);
+    })
+
+    fabric.loadSVGFromURL('../svg/Stickman.svg', function (objects, options) {
+        var obj = fabric.util.groupSVGElements(objects, options);
+        obj.scale(2);
+        obj.set({ left: 350, top: 400 });
+        obj.selectable = false;
+        canvas.add(obj);
+    })
 
 }
