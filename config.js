774a1d4ca0ecf03a00627f60de8e06928f64ad35
kosert <robert.kosakowski@student.put.poznan.pl>
2018-04-11 17:28:30 +0200

    config refactor

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
-module.exports = {
+var fs = require('fs')
+
+var defaults = {
     dbUrl: 'mongodb://localhost/StickArena',
-    sessionSecret: 'tududu'
+    sessionSecret: 'tududu',
+    disableMail: true,
+    mailConfig: null
 }
+
+if (!fs.existsSync('config.json')) {
+    var json = JSON.stringify(defaults)
+    fs.writeFileSync('config.json', json, { encoding: 'utf8' })
+}
+var contents = fs.readFileSync('config.json', { encoding: 'utf8' })
+var config = JSON.parse(contents)
+
+module.exports = config
