75e843e6e2fdc0386e7629e83f098a8a3083ad1f
kosert <robert.kosakowski@student.put.poznan.pl>
2018-05-08 17:50:00 +0200

    log upgrade

--- log.js
+++ log.js
@@ -1,16 +1,34 @@
 // utility log functions
 module.exports = {
 
-    info: function(message) {
-        console.log('\x1b[37m[INFO] \x1b[0m', message)
+    info: function (...messages) {
+        let msg = '\x1b[37m[INFO] '
+        messages.forEach(element => {
+            msg += element
+            msg += " "
+        })
+        msg += '\x1b[0m'
+        console.log(msg)
     },
 
-    warning: function(message) {
-        console.log('\x1b[1;33m[WARN] \x1b[0m', message)
+    warning: function (...messages) {
+        let msg = '\x1b[1;33m[WARN] '
+        messages.forEach(element => {
+            msg += element
+            msg += " "
+        })
+        msg += '\x1b[0m'
+        console.log(msg)
     },
 
-    error: function(message) {
-        console.log('\x1b[1;31m[ERROR] \x1b[0m', message) 
+    error: function (...messages) {
+        let msg = '\x1b[1;31m[ERROR] '
+        messages.forEach(element => {
+            msg += element
+            msg += " "
+        })
+        msg += '\x1b[0m'
+        console.log(msg)
     }
 }
 
