5e4655ce6e4bcaec94c1238225ed9f461f8bae97
kosert <robert.kosakowski@student.put.poznan.pl>
2018-05-08 18:32:21 +0200

    better defaults config handling

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ var defaults = {
     dbUrl: 'mongodb://localhost/StickArena',
     sessionSecret: 'tududu',
     disableMail: true,
-    mailConfig: null
+    mailConfig: null,
+    skipWeaponChanges: false
 }
 
 /*
@@ -23,7 +24,8 @@ if (!fs.existsSync('config.json')) {
     var json = JSON.stringify(defaults)
     fs.writeFileSync('config.json', json, { encoding: 'utf8' })
 }
+
 var contents = fs.readFileSync('config.json', { encoding: 'utf8' })
-var config = JSON.parse(contents)
+var config = Object.assign(defaults, JSON.parse(contents))
 
 module.exports = config
